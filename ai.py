e989f698e145ef1be16a87abab7bbe79e60e458c
Shaunak Turaga <shaunakturaga@gmail.com>
2023-03-07 17:38:09 -0800

    built email prettier using open ai

---
+++ ai.py
@@ -0,0 +1,41 @@
+from langchain import PromptTemplate
+from langchain.llms import OpenAI
+from decouple import config
+import os
+
+os.environ["OPENAI_API_KEY"] = config('KEY')
+
+template = """
+    Below is an email that may be poorly worded.
+    Your goal is to:
+    - Properly format the email
+    - Convert the input text to a specified tone
+    - Convert the input text to a specified dialect
+    Here are some examples different Tones:
+    - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
+    - Informal: Went to Barcelona for the weekend. Lots to tell you.  
+    Here are some examples of words in different dialects:
+    - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
+    - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
+    Example Sentences from each dialect:
+    - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
+    - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
+    Please start the email with a warm introduction. Add the introduction if you need to.
+    
+    Below is the email, tone, and dialect:
+    TONE: {tone}
+    DIALECT: {dialect}
+    EMAIL: {email}
+    
+    YOUR {dialect} RESPONSE:
+"""
+
+prompt = PromptTemplate(
+    input_variables=["tone","dialect","email"],
+    template = template,
+)
+
+def load_LLM():
+    llm = OpenAI(temperature=0.5)
+    return llm
+
---
+++ main.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from ai import load_LLM
+
+llm = load_LLM()
+
+st.set_page_config(page_title="Email Beautifier", page_icon=":robot")
+st.header("Email Beautifier")
+
+col1, col2 = st.columns(2)
+
+## Set Up Header Section
+with col1:
+    st.markdown("This is a tool that helps you improve the quality of your written emails using AI. This platform was built using Streamlit, Python, ChatGPT, and a couple small libraries to make the magic happen.")
+    st.markdown("If you have any questions about the implementation, please reach out to me on Twitter @shaunakturaga.")
+with col2:
+    st.image("https://media1.giphy.com/media/kjpKQ8wXVVocN5IIyK/giphy.gif?cid=ecf05e47uh7j01cwdtx479ag9h4yf3ifajkjxt5apgpu89jl&rid=giphy.gif&ct=g", width=300)
+
+## Set Up Input Area
+
+st.markdown("## Enter Your Email to Convert")
+col1, col2 = st.columns(2)
+
+with col1:
+        option_tone = st.selectbox(
+            'Which tone would you like your email to have?',
+            ('Formal', 'Informal')
+        )
+
+with col2:
+        option_dialect = st.selectbox(
+            'Which English dialect would you like?',
+            ('American English', 'British English','Indian English')
+        )
+
+def get_text():
+    input_text = st.text_area(label="Please enter your email content",placeholder="Please enter your email content", key="email_content")
+    return input_text
+
+email_content = get_text()
+
+st.markdown("### Your Converted Email")
+
+if email_content:
+    prompt_with_email = prompt.format(tone=option_tone,dialect=option_dialect,email=email_content)
+    formatted_email = llm(prompt_with_email)
+    st.write(formatted_email)
+
+
